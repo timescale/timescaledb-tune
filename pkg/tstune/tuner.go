f51ffbddb99a51ab3edd6f52fee50195da855edf
Rob Kiefer <rob.kiefer@gmail.com>
2020-11-24 15:47:48 -0500

    Release 0.10.0

--- pkg/tstune/tuner.go
+++ pkg/tstune/tuner.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// Version is the version of this library
-	Version = "0.9.0"
+	Version = "0.10.0"
 
 	errCouldNotExecuteFmt  = "could not execute `%s --version`: %v"
 	errUnsupportedMajorFmt = "unsupported major PG version: %s"
