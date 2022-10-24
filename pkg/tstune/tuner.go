0c4e8abbdf39787aaf3bf20863c23e79a63c5fac
Sven Klemm <sven@timescale.com>
2022-10-24 17:48:23 +0200

    Release 0.14.2
    This release adds support for postgres version specific config
    file path to timescaledb-tune

--- pkg/tstune/tuner.go
+++ pkg/tstune/tuner.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// Version is the version of this library
-	Version = "0.14.1"
+	Version = "0.14.2"
 
 	errCouldNotExecuteFmt  = "could not execute `%s --version`: %v"
 	errUnsupportedMajorFmt = "unsupported major PG version: %s"
