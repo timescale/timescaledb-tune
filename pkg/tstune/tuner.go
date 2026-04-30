39379ebbd984fa1248ffe1f22ddbfe82b8e391ff
Sven Klemm <sven@timescale.com>
2026-04-30 10:41:14 +0200

    Bump version to 0.19.0

--- pkg/tstune/tuner.go
+++ pkg/tstune/tuner.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// Version is the version of this library
-	Version = "0.18.1"
+	Version = "0.19.0"
 
 	errCouldNotExecuteFmt  = "could not execute `%s --version`: %v"
 	errUnsupportedMajorFmt = "unsupported major PG version: %s"
